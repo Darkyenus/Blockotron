65244c60d407d8f54c6c3c34501d59acde11981a
Chris Browne <yoda2031@gmail.com>
2016-04-18 16:52:02 +0200

    Initial window size set to 70% of user's screen size, instead of an arbitrary small box in the middle.

--- src/main/java/darkyenus/blockotron/Blockotron.java
+++ src/main/java/darkyenus/blockotron/Blockotron.java
@@ -1,7 +1,9 @@
 package darkyenus.blockotron;
 
+import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Graphics;
 import darkyenus.blockotron.client.Game;
 
 /**
@@ -11,8 +13,10 @@ public class Blockotron {
     public static void main(String[] args){
         final Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
         final int msaa = 0;
+        double scaleBy = 0.7;
         config.setBackBufferConfig(8, 8, 8, 8, 16, 0, msaa);
-
+        Graphics.DisplayMode desktopMode = Lwjgl3ApplicationConfiguration.getDisplayMode();
+        config.setWindowedMode((int) (desktopMode.width * scaleBy), (int) (desktopMode.height * scaleBy));
         new Lwjgl3Application(new Game(), config);
     }
 }
